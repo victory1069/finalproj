ad1f4c3f01bdd2929fa22c8f0ccda9c405dc4d21
Johnwav <johnwav@users.noreply.github.com>
2024-06-22 22:53:08 +0100

    feat: import transfer

--- finalproj-frontend/src/App.jsx
+++ finalproj-frontend/src/App.jsx
@@ -17,27 +17,8 @@ import Wallet from "./Comp/Wallet";
 import EnterCardDetails from "./Comp/EnterCardDetails";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "./contexts/UserContext";
-import Transfer from "./Comp/Transfer";
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useLocation,
-} from "react-router-dom";
-import AuthPage from "./Comp/AuthPage";
-import SignupPage from "./Comp/SignupPage";
-import Dashboard from "./Comp/Dashboard";
-import ProfileUpdate from "./Comp/ProfileUpdate";
-import ServicePayment from "./Comp/ServicePayment";
-import OtpVerificationPage from "./Comp/OtpverificationPage";
-import ForgotPassword from "./Comp/ForgotPassword";
-import Wallet from "./Comp/Wallet";
-import EnterCardDetails from "./Comp/EnterCardDetails";
-import { useNavigate } from "react-router-dom";
-import { useUser } from "./contexts/UserContext";
 import axiosInstance from "./axiosConifg";
+import Transfer from "./Comp/Transfer";
 
 function App() {
   const navigate = useNavigate();
