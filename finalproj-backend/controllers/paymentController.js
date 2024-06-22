ca6252b70ec5076bceb3aabe97ddc2fce39eae5c
Johnwav <johnwav@users.noreply.github.com>
2024-06-22 22:50:11 +0100

    feat: send to user method

--- finalproj-backend/controllers/paymentController.js
+++ finalproj-backend/controllers/paymentController.js
@@ -1,28 +1,62 @@
-exports.payWithCard = async (req, res) => {
-    const { cardDetails, amount } = req.body;
+const User = require("../models/User");
 
-    // Integrate with a payment gateway here, e.g., Stripe
-    // Assuming payment is successful:
-    res.json({ msg: 'Payment successful', amount });
+exports.payWithCard = async (req, res) => {
+  const { cardDetails, amount } = req.body;
+
+  // Integrate with a payment gateway here, e.g., Stripe
+  // Assuming payment is successful:
+  res.json({ msg: "Payment successful", amount });
 };
 
 exports.payWithWallet = async (req, res) => {
-    const { amount } = req.body;
+  const { amount } = req.body;
 
-    try {
-        const user = await User.findById(req.user.id);
+  try {
+    const user = await User.findById(req.user.id);
 
-        if (user.walletBalance < amount) {
-            return res.status(400).json({ msg: 'Insufficient balance' });
-        }
-
-        user.walletBalance -= amount;
-        await user.save();
-
-        res.json({ msg: 'Payment successful', amount });
-
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+    if (user.walletBalance < amount) {
+      return res.status(400).json({ msg: "Insufficient balance" });
     }
+
+    user.walletBalance -= amount;
+    await user.save();
+
+    res.json({ msg: "Payment successful", amount });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.transferToUser = async (req, res) => {
+  const { amount, email: recipientEmail } = req.body;
+
+  console.log(req);
+
+  try {
+    const user = await User.findById(req.user.id);
+    const recipient = await User.findOne({ email: recipientEmail });
+
+    if (!user || !recipient) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    if (user.walletBalance < amount) {
+      return res.status(400).json({ msg: "Insufficient balance" });
+    }
+
+    user.walletBalance -= amount;
+    recipient.walletBalance += amount;
+
+    await user.save();
+    await recipient.save();
+
+    res.status(200).json({
+      msg: "Transaction successful",
+      info: `You sent ${amount} to ${recipientEmail}`,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 };
