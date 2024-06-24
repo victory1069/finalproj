2f2ebcdcc24642d89dfa71afac9577b00fcd2256
Johnwav <johnwav@users.noreply.github.com>
2024-06-24 20:12:18 +0100

    feat: import used module

--- finalproj-backend/app.js
+++ finalproj-backend/app.js
@@ -31,6 +31,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
