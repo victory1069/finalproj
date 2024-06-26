175a2396d50083aaa2a5b2edb9b1bce31242cdfd
Johnwav <johnwav@users.noreply.github.com>
2024-06-26 15:30:07 +0100

    fix: use className

--- finalproj-frontend/src/Comp/TransactionHistory.jsx
+++ finalproj-frontend/src/Comp/TransactionHistory.jsx
@@ -3,26 +3,29 @@ import { FiArrowLeft } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../contexts/UserContext";
 import axiosInstance from "../axiosConifg";
-import { toast } from 'sonner'
+import { toast } from "sonner";
 import { formatCurrency } from "../utilities";
 
 const TransactionHistory = () => {
   //use transaction id for key
   const navigate = useNavigate();
   const [transactions, setTransactions] = useState([]);
-  const { user } = useUser()
+  const { user } = useUser();
 
   useEffect(() => {
     if (!user) return;
-    axiosInstance.get("/transactions",
-      { headers: { "x-auth-token": user._id } }
-    ).then(res => {
-      setTransactions(res.data.data.transactions)
-    }).catch(err => {
-      console.error(err)
-      toast.error(`Unable to get transactions: ${err.message} /n Please contact an administrator`)
-    })
-  }, [user])
+    axiosInstance
+      .get("/transactions", { headers: { "x-auth-token": user._id } })
+      .then((res) => {
+        setTransactions(res.data.data.transactions);
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error(
+          `Unable to get transactions: ${err.message} /n Please contact an administrator`
+        );
+      });
+  }, [user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white font-agrandir">
       <main className="text-center w-full max-w-md bg-white bg-opacity-25 rounded-lg shadow-lg">
@@ -37,19 +40,31 @@ const TransactionHistory = () => {
         </div>
 
         <div className="flex flex-col gap-6 max-h-96 overflow-auto">
-          {transactions.length > 0 ? transactions.map((transaction, index, array) => (
-            <div
-              key={transaction._id + index}
-              className={`flex justify-between items-center px-6 ${index !== array.length - 1 ? "border-b" : ""}`}
-            >
-              <div className="flex flex-col items-start gap-3 py-2">
-                <div>
-                  {user.email === transaction.senderEmail ? transaction.descriptionSender : transaction.descriptionRecipient}
+          {transactions.length > 0 ? (
+            transactions.map((transaction, index, array) => (
+              <div
+                key={transaction._id + index}
+                className={`flex justify-between items-center px-6 ${
+                  index !== array.length - 1 ? "border-b" : ""
+                }`}
+              >
+                <div className="flex flex-col items-start gap-3 py-2">
+                  <div>
+                    {user.email === transaction.senderEmail
+                      ? transaction.descriptionSender
+                      : transaction.descriptionRecipient}
+                  </div>
+                </div>
+                <div className="text-1xl font-bold py-2 ">
+                  {formatCurrency(transaction.amount / 100)}
                 </div>
               </div>
-              <div className="text-1xl font-bold py-2 ">{formatCurrency(transaction.amount / 100)}</div>
+            ))
+          ) : (
+            <div className="flex justify-center items-center py-6 text-xl">
+              You have no transactions right now
             </div>
-          )) : <div class="flex justify-center items-center py-6 text-xl">You have no transactions right now</div>}
+          )}
         </div>
       </main>
     </div>
